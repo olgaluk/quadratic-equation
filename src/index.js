0ecfe305ad6f6507054e777e91dd6dc11c0533f3
Olga Luk <olga_ivanova_86@mail.ru>
2018-09-16 20:35:37 +0300

    solution of equation

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function solveEquation(equation) {
   // your implementation
+  var firstString = equation;
+  var arrayString = firstString.split(" ");
+  var a = +arrayString[0];
+  if (arrayString[3] === '+') {
+    var b = +arrayString[4];
+  } else {
+    b = -(+arrayString[4]);
+  };
+  if (arrayString[7] === '+') {
+    var c = +arrayString[8];
+  } else {
+    c = -(+arrayString[8]);
+  };
+  var x1 = Math.round(-b + Math.sqrt(b*b - 4*a*c))/(2*a);
+  var x2 = Math.round(-b - Math.sqrt(b*b - 4*a*c))/(2*a);
+  var arrFinish = [x1, x2];
+  arrFinish.sort((a,b) => a -b);
+  return arrFinish;
 }
